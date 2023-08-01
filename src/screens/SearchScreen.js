8ddc77a6f0a55d971a8d84cd2d5cfe2d247111ca
Stephen Moore <judo2000@comcast.net>
2023-08-01 15:17:16 -0600

    Create custom hook useResults.js

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,33 +1,19 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMsg, setErrorMsg] = useState('');
-
-  const searchApi = async () => {
-    try {
-      setErrorMsg('');
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term,
-          location: 'denver',
-        },
-      });
-
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMsg('Something went wrong');
-    }
-  };
+  const [searchApi, results, errorMsg] = useResults();
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
       {errorMsg ? <Text>{errorMsg}</Text> : null}
       <Text>We have found {results.length} results.</Text>
     </View>
